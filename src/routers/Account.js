6c9c3801af4b5951311d3d9473b0a4e6069d43dd
JongMin Kim <87972252+Guksu@users.noreply.github.com>
2021-10-30 19:29:40 +0900

    Update Account.js

--- src/routers/Account.js
+++ src/routers/Account.js
@@ -31,7 +31,7 @@ const Account = () => {
   const [expenseSelect, setExpenseSelect] = useState("기타등등");
 
   const onSubmitAccount = async (e) => {
-    await Axios.post("http://localhost:3306/account", {
+    await Axios.post("https://save-money-back.herokuapp.com/account", {
       date: date,
       profit: parseInt(profit.replace(/,/g, "")),
       expense: parseInt(expense.replace(/,/g, "")),
